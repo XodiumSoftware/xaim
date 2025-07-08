3f809b9c7cea2a7f0c2d06d2fefd0c5f561f72a0
illyrius666 <fitimq@live.nl>
2025-07-08 15:42:53 +0200

    Add StarCoder fine-tuning script with HuggingFace Transformers and update dependencies

--- src/main.py
+++ src/main.py
@@ -1,8 +1,44 @@
 #   Copyright (c) 2025. Xodium.
 #   All rights reserved.
 
-class Main:
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    TextDataset,
+    Trainer,
+    TrainingArguments,
+)
 
+model_name = "bigcode/starcoder"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
 
-if __name__ == "__main__":
-    Main()
+train_dataset = TextDataset(
+    tokenizer=tokenizer,
+    file_path="data/train.txt",
+    block_size=128,
+)
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer,
+    mlm=False,
+)
+
+training_args = TrainingArguments(
+    output_dir="./finetuned_starcoder",
+    overwrite_output_dir=True,
+    num_train_epochs=1,
+    per_device_train_batch_size=1,
+    save_steps=10_000,
+    save_total_limit=2,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_dataset,
+)
+
+trainer.train()
